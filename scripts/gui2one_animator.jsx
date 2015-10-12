36bca09c9eb1698f95dbf04c4fcc8f2599e222aa
gui2one <guillaume.rouault001@orange.fr>
2015-10-12 14:34:54 +0200

    sdfsdf

---
+++ scripts/gui2one_animator.jsx
@@ -0,0 +1,62 @@
+﻿function findCompByName(name){
+    var myCompName = name;
+    var myComp = null;
+    for (var i = 1; i <= app.project.numItems; i ++){
+      if ((app.project.item(i) instanceof CompItem) && (app.project.item(i).name == myCompName)){
+        myComp = app.project.item(i);
+        return myComp;
+      }
+    }
+    if (myComp == null){
+      alert("no luck");
+    }else{
+       alert ("got it");
+    }    
+}
+
+function createUI(thisObj) {
+    var myPanel = thisObj;
+
+    addKeysBtn =  myPanel.add("button",[10,10,100,30] ,"Add Keys");
+    myPanel.add("button",[10,50,100,70] ,"btn2");
+
+    addKeysBtn.onClick = function(){
+        var layer = app.project.activeItem.selectedLayers[0]
+        var posProp = layer.property("Position");
+        var startPos = posProp.value;
+        $.writeln(startPos[0]);
+        var posK1 = posProp.addKey(layer.time);
+        $.writeln(posK1 + " : posK1");
+        posProp.setInterpolationTypeAtKey(posK1,KeyframeInterpolationType.BEZIER); 
+        
+        var easeIn = new KeyframeEase(0.0,100.0);
+        var easeOut = new KeyframeEase(0.75, 85);
+        posProp.setTemporalEaseAtKey(posK1,[easeIn] ,[easeOut]);  
+        
+        var posK2 = posProp.addKey(layer.time - 0.5);
+        $.writeln(posK2 + " : posK2");
+        posProp.setValueAtTime(layer.time - 0.5, [startPos[0]-20,startPos[1]]);
+        posProp.setInterpolationTypeAtKey(posK2,KeyframeInterpolationType.BEZIER);        
+        var easeIn2 = new KeyframeEase(0.0,100.0);
+        var easeOut2 = new KeyframeEase(0, 0.1);
+        posProp.setTemporalEaseAtKey(posK1,[easeIn2] ,[easeOut2]);        
+        
+        var opacityProp = layer.property("Opacity");
+        var startOpac = opacityProp.value;
+        var opacK1 = opacityProp.addKey(layer.time);
+        var opacK2 =opacityProp.addKey(layer.time - 0.5);
+        opacityProp.setValueAtTime(layer.time - 0.5, 0.0);        
+       
+       
+        for( o in app.project.activeItem){
+            
+//                $.writeln( app.project.activeItem[o] );
+        }
+    }
+    
+
+    return myPanel;
+
+}
+
+var animatorPanel = createUI(this);
---
+++ scripts/myCustomPanel.jsx
@@ -0,0 +1,55 @@
+﻿function findCompByName(name){
+    var myCompName = name;
+    var myComp = null;
+    for (var i = 1; i <= app.project.numItems; i ++){
+      if ((app.project.item(i) instanceof CompItem) && (app.project.item(i).name == myCompName)){
+        myComp = app.project.item(i);
+        return myComp;
+      }
+    }
+    if (myComp == null){
+      alert("no luck");
+    }else{
+       alert ("got it");
+    }    
+}
+
+
+function createUI(thisObj) {
+    var myPanel = thisObj;
+
+    populateBtn =  myPanel.add("button",[10,10,100,30] ,"populate");
+    myPanel.add("button",[10,50,100,70] ,"btn2");
+    
+    populateBtn.onClick = function(){
+        var  workComp = findCompByName("nulls_02");
+        var cubeComp = findCompByName("cube_1");
+            for(var i=1; i<28   ; i++){
+                    $.writeln(i);
+                    var nullLayer = workComp.layers.byName("null"+i);
+                    var cubeLayer = workComp.layers.add(cubeComp);
+                   
+                    cubeLayer.threeDLayer = true;
+
+                    var nullX = nullLayer.property("Position").value[0];
+                    $.writeln(nullX);
+                    var nullY = nullLayer.property("Position").value[1];
+                    var nullZ = nullLayer.property("Position").value[2];
+                    
+                    var scale = 100.0 / 5.12;
+                    cubeLayer.parent = nullLayer;
+                    cubeLayer.setParentWithJump(nullLayer);                  
+                    cubeLayer.property("Position").setValue([0,0,0]);
+                    cubeLayer.property("Scale").setValue([scale,scale,scale]);
+                    cubeLayer.collapseTransformation = true;
+                }
+           
+           
+            
+    }
+    return myPanel;
+}
+
+$.writeln(this);
+
+var myToolsPanel = createUI(this);
