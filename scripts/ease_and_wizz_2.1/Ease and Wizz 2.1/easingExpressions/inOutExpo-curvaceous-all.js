872300c835aa325a262526792d2686279bbded25
gui2one <guillaume.rouault001@orange.fr>
2015-10-12 14:26:28 +0200

    first commit

---
+++ scripts/ease_and_wizz_2.1/Ease and Wizz 2.1/easingExpressions/inOutExpo-curvaceous-all.js
@@ -0,0 +1,53 @@
+// Ease and Wizz 2.1 : Curvaceous : inOutExpo : all keyframes
+// Ian Haigh (http://ianternet.net/ease-and-wizz/)
+// Last built: 2015-08-07T15:45:18+10:00
+
+// some defaults
+var p = 0.81;		// period for elastic
+var a = 50;		// amplitude for elastic
+var s = 1.70158;	// overshoot amount for "back"
+
+function easeandwizz_inOutExpo(t, b, c, d) {
+	if (t==0) return b;
+	if (t==d) return b+c;
+	if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+	return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+}
+
+
+
+function easeAndWizz() {
+	var n = 0;
+	if (numKeys > 0) {
+		n = nearestKey(time).index;
+		if (key(n).time > time)	{ n-- }
+	}
+
+	// after the first two keys, yet before the last two, just do nothing
+	if (n > 1 && n < numKeys -1 ) {
+		return null;
+	}
+
+	try {
+		var key1 = key(n);
+		var key2 = key(n+1);
+	} catch(e) {
+		return null;
+	}
+	
+	t = time - key1.time;
+	d = key2.time - key1.time;
+
+	sX = key1.time;
+	eX = key2.time - key1.time;
+
+
+	if ((time < key1.time) || (time > key2.time)) {
+		return null;
+	} else {
+		return valueAtTime(easeandwizz_inOutExpo(t, sX, eX, d));
+	}
+}
+
+(easeAndWizz() || value);
+
---
+++ scripts/ease_and_wizz_2.1/Ease and Wizz 2.1/easingExpressions/inOutExpo-curvaceous-startEnd.js
@@ -0,0 +1,42 @@
+// Ease and Wizz 2.1 : Curvaceous : inOutExpo : start and end keyframes
+// Ian Haigh (http://ianternet.net/ease-and-wizz/)
+// Last built: 2015-08-07T15:45:18+10:00
+
+// some defaults
+var p = 0.81;		// period for elastic
+var a = 50;		// amplitude for elastic
+var s = 1.70158;	// overshoot amount for "back"
+
+function easeandwizz_inOutExpo(t, b, c, d) {
+	if (t==0) return b;
+	if (t==d) return b+c;
+	if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+	return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+}
+
+
+
+function easeAndWizz() {
+	try {
+		var key1 = key(1);
+		var key2 = key(numKeys);
+	} catch(e) {
+		return null;
+	}
+	
+	t = time - key1.time;
+	d = key2.time - key1.time;
+
+	sX = key1.time;
+	eX = key2.time - key1.time;
+
+
+	if ((time < key1.time) || (time > key2.time)) {
+		return null;
+	} else {
+		return valueAtTime(easeandwizz_inOutExpo(t, sX, eX, d));
+	}
+}
+
+(easeAndWizz() || value);
+
