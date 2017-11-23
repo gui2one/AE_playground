824c2dae8ada1e5496fea022852ef889122defda
gui2one <guillaume.rouault.fx@gmail.com>
2017-11-23 18:16:36 +0100

    mke full strip script

--- launched_scripts/make_full_strip.jsx
+++ launched_scripts/make_full_strip.jsx
@@ -6,13 +6,34 @@ if(selection.length > 0){
     if(selection[0] instanceof FootageItem)
     {
     
+        app.beginUndoGroup("create strip comp");
+
         imgWidth = selection[0].width;
         imgHeight = selection[0].height;
 
         numStrips = parseInt(prompt("Enter the number of strips.", "4"));
+
+        stripWidth = imgWidth * numStrips;
+        stripHeight = imgHeight / numStrips;
+
         if(numStrips){
 
-            alert (numStrips);
+            var myComp = app.project.items.addComp("000_new_comp", stripWidth,  stripHeight, 1.0, 10.0, 25.0);
+            
+            for(var i=0; i< numStrips; i++){
+                
+                var layer1 = myComp.layers.add(selection[0]);
+                
+                layer1.property('anchorPoint').setValue([0,i * stripHeight]);
+                layer1.property('position').setValue([i * stripWidth / numStrips,0]);
+
+            }
+
+            myComp.openInViewer();
+            //alert (numStrips);
+
+
+            app.endUndoGroup();
         }else{
             alert ("not a number... \nenter a Number");
         }
